b8db46a0442aae43ec6c6c54e95f1a1a7702e897
Emanuel Machado <emanuel@emachado.pt>
2023-08-18 22:24:42 +0100

    No guards for user login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,8 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
-import { AuthGuard } from '@nestjs/passport';
-
 interface LoginUser {
-  email: string;
+  username: string;
   password: string;
 }
 
@@ -13,7 +11,6 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('auth/login')
-  @UseGuards(AuthGuard('local'))
   async login(@Request() req: Request & { user: LoginUser }) {
     return this.authService.login(req.body);
   }
