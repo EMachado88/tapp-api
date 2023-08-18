d03977504f5b02d4c5d7aed35b86e9b8dc4f3cb0
Emanuel Machado <emanuel@emachado.pt>
2023-08-18 20:24:28 +0100

    Enabled CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(port);
 }
 bootstrap();
